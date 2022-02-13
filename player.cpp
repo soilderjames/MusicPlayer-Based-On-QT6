50124867c5d0e22ca08827fcba5ef9f623bea08d
soilderjames <soilderjames@163.com>
2022-02-13 13:24:23 +0800

    Update player.cpp
    添加后缀名判断，避免打开非音频文件

--- player.cpp
+++ player.cpp
@@ -1,16 +1,8 @@
 #include "player.h"
-QMediaPlayer player;
-QAudioOutput audioOutput;
-QStringList Filelist;
-int music_num=0;
-int current_music_num=0;
-int value=0;
-m_info Musicinfo;
 QImage image;
 ablumimage::ablumimage()
     : QQuickImageProvider(QQuickImageProvider::Image)
 {
-
 }
 
 QImage ablumimage::requestImage(const QString &id, QSize *size, const QSize &requestedSize)
@@ -18,83 +10,145 @@ QImage ablumimage::requestImage(const QString &id, QSize *size, const QSize &req
     return image;
 }
 
+musicplayer::musicplayer(QObject *parent) : QObject(parent)
+{
+}
+musicplayer::~musicplayer()
+{
+}
+
 void musicplayer::next()
 {
-    if(current_music_num<music_num)
+    if (!shuffle)
     {
-        current_music_num++;
+        if (current_music_num < music_num)
+        {
+            current_music_num++;
+        }
+    }
+    else
+    {
+        current_music_num = shuffler.bounded(0, music_num);
+    }
+    if (player.playbackState() == 1)
+    {
         player.setSource(QUrl::fromLocalFile(Filelist.at(current_music_num)));
         player.play();
     }
-}
-
-void musicplayer::find()
-{
-    qDebug()<<"start find!";
-    QDir dir;
-    dir.setPath("*****DIR YOU WANT*****");
-    QDirIterator file(dir,QDirIterator::Subdirectories);
-    while (file.hasNext())
+    else
     {
-    file.next();
-    QFileInfo info=file.fileInfo();
-    if (info.isFile())
-    {
-    Filelist.append(info.absoluteFilePath());
-    music_num++;
+        player.setSource(QUrl::fromLocalFile(Filelist.at(current_music_num)));
     }
-    }  
 }
-
-void musicplayer::start()
+void musicplayer::start(QUrl url)
 {
-    player.setAudioOutput(&audioOutput);
-    player.setSource(QUrl::fromLocalFile(Filelist.at(current_music_num)));
-    player.play();
-    connect(&player,&QMediaPlayer::mediaStatusChanged,this,&musicplayer::end_next);
-    connect(&player,&QMediaPlayer::durationChanged,this,&musicplayer::get_metadata);
-    connect(&player,&QMediaPlayer::positionChanged,this,&musicplayer::get_pos);
-    qDebug()<<"start!";
+    const QStringList filefilter = {"*.mp3", "*.wav", "*.m4a", "*.wma", "*.flac", "*.aac", "*.ape"};
+    if (player.mediaStatus() == 0)
+    {
+        qDebug() << "start find!";
+        QDir dir;
+        dir.setPath(url.toString());
+        QDirIterator file(dir.absolutePath(), filefilter, QDir::Files, QDirIterator::Subdirectories);
+        while (file.hasNext())
+        {
+            file.next();
+            QFileInfo info = file.fileInfo();
+            if (info.isFile())
+            {
+                Filelist.append(info.absoluteFilePath());
+                music_num++;
+            }
+        }
+        player.setAudioOutput(&audioOutput);
+        player.setSource(QUrl::fromLocalFile(Filelist.at(current_music_num)));
+        player.play();
+        connect(&player, &QMediaPlayer::mediaStatusChanged, this, &musicplayer::end_next);
+        connect(&player, &QMediaPlayer::durationChanged, this, &musicplayer::get_metadata);
+        connect(&player, &QMediaPlayer::positionChanged, this, &musicplayer::get_pos);
+    }
+    else
+    {
+        qDebug() << "change dir!";
+        QDir dir;
+        dir.setPath(url.toString());
+        QDirIterator file(dir.absolutePath(), filefilter, QDir::Files, QDirIterator::Subdirectories);
+        Filelist.clear();
+        current_music_num = 0;
+        music_num = 0;
+        while (file.hasNext())
+        {
+            file.next();
+            QFileInfo info = file.fileInfo();
+            if (info.isFile())
+            {
+                Filelist.append(info.absoluteFilePath());
+                music_num++;
+            }
+        }
+        player.setSource(QUrl::fromLocalFile(Filelist.at(current_music_num)));
+        player.play();
+    }
 }
 
 void musicplayer::previous()
 {
-    if(current_music_num>0)
+    if (!shuffle)
     {
-        current_music_num--;
+        if (current_music_num > 0)
+        {
+            current_music_num--;
+        }
+    }
+    else
+    {
+        current_music_num = shuffler.bounded(0, music_num);
+    }
+    if (player.playbackState() == 1)
+    {
         player.setSource(QUrl::fromLocalFile(Filelist.at(current_music_num)));
         player.play();
+    }
+    else
+    {
+        player.setSource(QUrl::fromLocalFile(Filelist.at(current_music_num)));
     }
 }
 
 void musicplayer::end_next()
 {
-    if(player.mediaStatus()==6)
+    if (player.mediaStatus() == 6&&loop==false)
     {
-        if(current_music_num<music_num)
+        if (!shuffle)
         {
-            current_music_num++;
-            player.setSource(QUrl::fromLocalFile(Filelist.at(current_music_num)));
-            player.play();
+            if (current_music_num < music_num)
+            {
+                current_music_num++;
+            }
         }
+        else
+        {
+            current_music_num = shuffler.bounded(0, music_num);
+        }
+        player.setSource(QUrl::fromLocalFile(Filelist.at(current_music_num)));
+        player.play();
     }
 }
 
 void musicplayer::get_metadata()
 {
-    QMediaMetaData info=player.metaData();
-    Musicinfo.Author=info.stringValue(info.Author);
-    Musicinfo.Title=info.stringValue(info.Title);
-    Musicinfo.AlbumTitle=info.stringValue(info.AlbumTitle);
-    image=info.value(info.ThumbnailImage).value<QImage>();
-    qDebug()<<image;
+    QMediaMetaData info = player.metaData();
+    Musicinfo.Author = info.stringValue(info.Author);
+    Musicinfo.Title = info.stringValue(info.Title);
+    Musicinfo.AlbumTitle = info.stringValue(info.AlbumTitle);
+    image = info.value(info.ThumbnailImage).value<QImage>();
+    qDebug() << image;
     emit metadata_get();
 }
 
 void musicplayer::get_pos()
 {
-    Musicinfo.Pos=player.position();
-    Musicinfo.Dur=player.duration();
+    Musicinfo.Pos = player.position();
+    Musicinfo.Dur = player.duration();
     emit pos_get();
 }
 
@@ -108,28 +162,36 @@ void musicplayer::play()
     player.play();
 }
 
-
 QVariantMap musicplayer::give_metadata(void)
 {
     QVariantMap map;
     map.clear();
-    map.insert("Author",Musicinfo.Author);
-    map.insert("Title",Musicinfo.Title);
-    map.insert("Ablum",Musicinfo.AlbumTitle);
+    map.insert("Author", Musicinfo.Author);
+    map.insert("Title", Musicinfo.Title);
+    map.insert("Ablum", Musicinfo.AlbumTitle);
     return map;
 }
 
 QVariantMap musicplayer::give_pos(void)
 {
-   QVariantMap Map;
-   Map.clear();
-   Map.insert("pos",player.position());
-   Map.insert("dur",player.duration());
-   return Map;
+    QVariantMap Map;
+    Map.clear();
+    Map.insert("pos", player.position());
+    Map.insert("dur", player.duration());
+    return Map;
 }
 
 void musicplayer::set_pos(qreal pos)
 {
-    player.setPosition(pos*player.duration());
+    player.setPosition(pos * player.duration());
 }
 
+void musicplayer::play_mode(int mode)
+{
+    switch(mode)
+    {
+    case 1: {shuffle =false;loop=true;player.setLoops(-1);qDebug() << "loop mode";break;}
+    case 2: {shuffle = false;loop=false;player.setLoops(1);qDebug() << "order mode";break;}
+    case 3: {shuffle = true;loop=false;player.setLoops(1);qDebug() << "shuffle mode";break;}
+    }
+}
